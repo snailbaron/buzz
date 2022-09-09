766a98652eb148eae719bbfbbc3a58e88c9812ae
snailbaron <snailbaron@gmail.com>
2022-09-09 04:42:29 +0300

    Introduce ECS for game objects

--- src/buzz/main.cpp
+++ src/buzz/main.cpp
@@ -3,6 +3,7 @@
 
 #include "build-info.hpp"
 #include "tempo.hpp"
+#include "thing.hpp"
 #include "ve.hpp"
 
 #include <algorithm>
@@ -17,22 +18,19 @@ template <class T> struct XYModel {
 };
 using XYVector = ve::Vector<XYModel, float>;
 
-// NOTE: just individual structs before ECS introduction
-
-struct Hero {
-    static constexpr float accelerationTime = 0.2f;
-    static constexpr float decelerationTime = 0.2f;
-    static constexpr float maxSpeed = 500.f;
-
+struct WorldLocation {
     XYVector position;
+    float radius = 0.f;
+};
+
+struct WorldMovement {
     XYVector velocity;
-    float radius = 50.f;
+    float maxSpeed = 0.f;
+    float accelerationTime = 0.f;
+    float decelerationTime = 0.f;
 };
 
-struct Tree {
-    XYVector position;
-    float radius = 50.f;
-};
+struct Control {};
 
 struct KeyboardControllerState {
     bool leftPressed = false;
@@ -58,44 +56,35 @@ enum class MoveSpeed {
     Walk,
 };
 
-struct HeroSprite {
-    const std::map<std::tuple<MoveDirection, MoveSpeed>, std::vector<SDL_Rect>> frames {
-        {{MoveDirection::Right, MoveSpeed::Walk}, {
-            {.x = 0, .y = 0, .w = 16, .h = 16},
-            {.x = 16, .y = 0, .w = 16, .h = 16},
-        }},
-        {{MoveDirection::Left, MoveSpeed::Walk}, {
-            {.x = 0, .y = 16, .w = 16, .h = 16},
-            {.x = 16, .y = 16, .w = 16, .h = 16},
-        }},
-        {{MoveDirection::Down, MoveSpeed::Walk}, {
-            {.x = 0, .y = 32, .w = 16, .h = 16},
-            {.x = 16, .y = 32, .w = 16, .h = 16},
-        }},
-        {{MoveDirection::Up, MoveSpeed::Walk}, {
-            {.x = 0, .y = 48, .w = 16, .h = 16},
-            {.x = 16, .y = 48, .w = 16, .h = 16},
-        }},
-        {{MoveDirection::Right, MoveSpeed::Stand}, {
-            {.x = 0, .y = 64, .w = 16, .h = 16},
-            {.x = 16, .y = 64, .w = 16, .h = 16},
-        }},
-        {{MoveDirection::Left, MoveSpeed::Stand}, {
-            {.x = 0, .y = 80, .w = 16, .h = 16},
-            {.x = 16, .y = 80, .w = 16, .h = 16},
-        }},
-        {{MoveDirection::Down, MoveSpeed::Stand}, {
-            {.x = 0, .y = 96, .w = 16, .h = 16},
-            {.x = 16, .y = 96, .w = 16, .h = 16},
-        }},
-        {{MoveDirection::Up, MoveSpeed::Stand}, {
-            {.x = 0, .y = 112, .w = 16, .h = 16},
-            {.x = 16, .y = 112, .w = 16, .h = 16},
-        }},
-    };
+struct DirectionalSprite {
+    DirectionalSprite(SDL_Texture* texture)
+        : texture(texture)
+    {
+        for (auto moveSpeed : {MoveSpeed::Walk, MoveSpeed::Stand}) {
+            for (auto moveDirection : {
+                    MoveDirection::Right,
+                    MoveDirection::Left,
+                    MoveDirection::Down,
+                    MoveDirection::Up}) {
+                int i = frames.size();
+                frames.emplace(
+                    std::tuple<MoveDirection, MoveSpeed>{moveDirection, moveSpeed},
+                    std::vector<SDL_Rect>
+                    {
+                        SDL_Rect{.x = 0, .y = 16 * i, .w = 16, .h = 16},
+                        SDL_Rect{.x = 16, .y = 16 * i, .w = 16, .h = 16},
+                    });
+            }
+        }
+    }
+
     const size_t frameNumber = 2;
     static constexpr int scale = 10;
     static constexpr float minWalkSpeed = 50.f;
+
+    SDL_Texture* texture = nullptr;
+    std::map<std::tuple<MoveDirection, MoveSpeed>, std::vector<SDL_Rect>> frames;
+
     MoveDirection direction = MoveDirection::Down;
     size_t frame = 0;
     SDL_FRect position {.x = 0, .y = 0, .w = 16 * scale, .h = 16 * scale};
@@ -103,6 +92,41 @@ struct HeroSprite {
 
 int main()
 {
+    auto ecs = thing::EntityManager{};
+
+    auto hero = ecs.createEntity();
+    {
+        ecs.add(hero, WorldLocation{
+            .position = {0, 0},
+            .radius = 50.f,
+        });
+        ecs.add(hero, WorldMovement{
+            .velocity = {0, 0},
+            .maxSpeed = 500.f,
+            .accelerationTime = 0.2f,
+            .decelerationTime = 0.2f,
+        });
+        ecs.add(hero, Control{});
+    }
+
+    auto tree1 = ecs.createEntity();
+    {
+        ecs.add(tree1, WorldLocation{
+            .position = {600.f, -100.f},
+            .radius = 50,
+        });
+    }
+
+    auto tree2 = ecs.createEntity();
+    {
+        ecs.add(tree2, WorldLocation{
+            .position = {200.f, -300.f},
+            .radius = 50,
+        });
+    }
+
+    auto controller = KeyboardControllerState{};
+
     const auto sdlFlags = SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS;
     sdlCheck(SDL_InitSubSystem(sdlFlags));
     check(IMG_Init(IMG_INIT_PNG) == IMG_INIT_PNG);
@@ -117,12 +141,7 @@ int main()
     SDL_Texture* treeTexture = IMG_LoadTexture(
         renderer, (SOURCE_ROOT / "assets"/ "tree.png").c_str());
 
-    auto hero = Hero{};
-    auto controller = KeyboardControllerState{};
-    auto heroSprite = HeroSprite{};
-
-    auto tree1 = Tree{.position = {600.f, -100.f}};
-    auto tree2 = Tree{.position = {200.f, -300.f}};
+    auto heroSprite = DirectionalSprite{heroTexture};
 
     auto timer = tempo::FrameTimer{60};
     auto animationMetronome = tempo::Metronome{3};
@@ -132,9 +151,12 @@ int main()
             if (e.type == SDL_QUIT) {
                 done = true;
             } else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_q &&
-                    e.key.keysym.mod == 0 && e.key.repeat == 0) {
+                    (e.key.keysym.mod & (KMOD_CTRL | KMOD_ALT | KMOD_SHIFT)) == 0 &&
+                    e.key.repeat == 0) {
                 done = true;
-            } else if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
+            } else if ((e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) &&
+                    e.key.repeat == 0) {
+
                 if (e.key.keysym.sym == SDLK_a) {
                     controller.leftPressed = (e.type == SDL_KEYDOWN);
                 } else if (e.key.keysym.sym == SDLK_d) {
@@ -152,48 +174,78 @@ int main()
 
         if (int framesPassed = timer(); framesPassed > 0) {
             // update world
+            const double delta = timer.delta();
             for (int i = 0; i < framesPassed; i++) {
-                auto acceleration = hero.maxSpeed / hero.accelerationTime;
-                auto deceleration = hero.maxSpeed / hero.decelerationTime;
+                auto acceleration = [] (const WorldMovement& m) {
+                    return m.maxSpeed / m.accelerationTime;
+                };
+                auto deceleration = [] (const WorldMovement& m) {
+                    return m.maxSpeed / m.decelerationTime;
+                };
 
-                hero.velocity += controller.control() *
-                    (acceleration + deceleration) * timer.delta();
-
-                auto speed = length(hero.velocity);
-                if (speed > 0) {
-                    auto targetSpeed = std::clamp(
-                        static_cast<float>(speed - deceleration * timer.delta()),
-                        0.f,
-                        hero.maxSpeed);
-                    hero.velocity *= targetSpeed / speed;
+                // update acceleration on controlled objects
+                //
+                for (auto& e : ecs.entities<Control>()) {
+                    auto& movement = ecs.component<WorldMovement>(e);
+                    movement.velocity += controller.control() *
+                        (acceleration(movement) + deceleration(movement)) * delta;
                 }
 
-                hero.position += hero.velocity * timer.delta();
+                // update moving object positions
+                for (auto& e : ecs.entities<WorldMovement>()) {
+                    auto& location = ecs.component<WorldLocation>(e);
+                    auto& movement = ecs.component<WorldMovement>(e);
 
-                for (const auto& tree : {tree1, tree2}) {
-                    auto oof = hero.radius + tree.radius - length(hero.position - tree.position);
-                    if (oof > 0) {
-                        hero.position += oof * unit(hero.position - tree.position);
+                    auto speed = length(movement.velocity);
+                    if (speed > 0) {
+                        auto targetSpeed = std::clamp(
+                            static_cast<float>(speed - deceleration(movement) * delta),
+                            0.f,
+                            movement.maxSpeed);
+                        movement.velocity *= targetSpeed / speed;
+                    }
+
+                    location.position += movement.velocity * delta;
+                }
+
+                // check for collisions with a stupid loop
+                for (auto& movingEntity : ecs.entities<WorldMovement>()) {
+                    auto& movingEntityLocation = ecs.component<WorldLocation>(movingEntity);
+                    for (const auto& someObjectLocation : ecs.components<WorldLocation>()) {
+                        if (&movingEntityLocation == &someObjectLocation) {
+                            continue;
+                        }
+
+                        auto oof = movingEntityLocation.radius +
+                            someObjectLocation.radius -
+                            length(movingEntityLocation.position -
+                                someObjectLocation.position);
+                        if (oof > 0) {
+                            movingEntityLocation.position +=
+                                oof * unit(movingEntityLocation.position -
+                                    someObjectLocation.position);
+                        }
                     }
                 }
             }
 
             // update and present graphics
             {
-                // hack before events are introduced
-                heroSprite.position.x = hero.position.x - 8 * heroSprite.scale;
-                heroSprite.position.y = -hero.position.y + 8 * heroSprite.scale;
+                const auto& heroPosition = ecs.component<WorldLocation>(hero).position;
+                heroSprite.position.x = heroPosition.x - 80;
+                heroSprite.position.y = -heroPosition.y + 80;
 
-                auto heroSpeed = length(hero.velocity);
+                const auto& heroVelocity = ecs.component<WorldMovement>(hero).velocity;
+                auto heroSpeed = length(heroVelocity);
                 if (heroSpeed > 0) {
-                    if (1.5 * std::abs(hero.velocity.x) > std::abs(hero.velocity.y)) {
-                        if (hero.velocity.x > 0) {
+                    if (1.5 * std::abs(heroVelocity.x) > std::abs(heroVelocity.y)) {
+                        if (heroVelocity.x > 0) {
                             heroSprite.direction = MoveDirection::Right;
                         } else {
                             heroSprite.direction = MoveDirection::Left;
                         }
                     } else {
-                        if (hero.velocity.y > 0) {
+                        if (heroVelocity.y > 0) {
                             heroSprite.direction = MoveDirection::Up;
                         } else {
                             heroSprite.direction = MoveDirection::Down;
@@ -214,10 +266,11 @@ int main()
                 sdlCheck(SDL_RenderClear(renderer));
 
                 for (const auto& tree : {tree1, tree2}) {
+                    const auto& treePosition = ecs.component<WorldLocation>(tree).position;
                     float scale = 10.f;
                     auto position = SDL_FRect {
-                        .x = tree.position.x - 4 * scale,
-                        .y = -tree.position.y + 8 * scale,
+                        .x = treePosition.x - 4 * scale,
+                        .y = -treePosition.y + 8 * scale,
                         .w = 8 * scale,
                         .h = 16 * scale};
                     sdlCheck(SDL_RenderCopyF(
